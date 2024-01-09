e7921fdf36f6c4a2cfa56a0a5f9feae969df95cd
Daniel <danielowenraine@gmail.com>
2024-01-09 13:54:45 +0000

    updated the configuration to be more specific to the requirements, currently only server and database implemented, basic structure to expand is in place. needs more work but good place to commit + push. Also basic structure of application has been implemented with module files, will continue to expand from here

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,121 @@
-pub mod setup;
+use std::{io::{self, Write, Error}, fs::{File, self}};
+
+use database::Database;
+use server::Server;
+
+pub mod api;
+pub mod server;
+pub mod database;
+pub mod file_upload;
+pub mod payment;
+pub mod security;
+pub mod products;
+pub mod ui;
+
+pub fn run_wizard() -> io::Result<()> {
+    Server::give_users_server_options();    
+    let server_input = unwrap_input();
+    let server = server::Server::get_config(&server_input).to_owned();
+    println!("input for server {:?}", server);
+
+    // Database feature
+    database::Database::give_users_database_options();
+    let database_input = unwrap_input();
+    println!("input for database: {:?}", &database_input);
+
+    let database = database::Database::get_config(&database_input).to_owned();
+
+    // Create and write to the config file
+    let mut config_file = File::create("config.toml")?;
+    writeln!(config_file, "server = {:?}", server)?;
+    writeln!(config_file, "database = {:?}", database)?;
+    println!("Configuration saved to config.toml");
+
+    Ok(())
+}
+fn unwrap_input() -> String {    
+    let mut input = String::new();
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_owned() // Trims and returns the input
+}
+
+pub fn setup_check() -> io::Result<()> {
+    // Check if the config file exists
+    if !fs::metadata("config.toml").is_ok() {
+        println!("It seems that you are trying to run the project without having a config.toml file, lets fix this!");
+        println!("For context, the config.toml will tell the application which package you would like to use within your project.");
+        println!("Note that there are mandatory packages to allow for core functionality.");
+        // The config file doesn't exist, so run the setup wizard
+        if std::env::args().any(|arg| arg == "--setup-wizard") {
+            println!("Running setup wizard...");
+            run_wizard()?; // Run the setup_wizard module
+        } else {
+            let mut input = String::new();
+            println!("Config file not found. To run the setup wizard");
+            println!("If you'd like to do this yourself then you can use: `cargo run -- --setup-wizard`");
+            println!("Alternatively we can do this automatically? it's up to you... [y/N]");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut input).unwrap();
+            let start_setup_wizard = input.trim().eq_ignore_ascii_case("y");
+            input.clear();
+            if start_setup_wizard {
+                println!("Great, lets get started.");
+                run_wizard()?;
+            } else {
+                println!("Okay great I know it's sometimes nice to go through the commands manually to learn the process.");
+                println!("The system will now EXIT.");
+                println!("Remember to run the command: `cargo run -- --setup-wizard` to successfully build the application with some working features");
+                return Ok(())
+            }
+        }
+    } else {
+        println!("Wizard has already been setup, config file exists so lets initialize the configuration into the application:)");
+    }
+    Ok(())
+}
+
+pub fn run_configuration_setup() -> Result<Configuration, Error>{
+    // Read the configuration file
+    let config_contents = fs::read_to_string("config.toml")?;
+
+    // Variables to hold your configuration values
+    let mut server = String::new();
+    let mut database = String::new();
+
+    // Parse the configuration
+    for line in config_contents.lines() {
+        let parts: Vec<&str> = line.split('=').map(|s| s.trim()).collect();
+        if parts.len() == 2 {
+            match parts[0] {
+                "server" => server = parts[1].trim_matches('"').to_string(),
+                "database" => database = parts[1].trim_matches('"').to_string(),
+                _ => continue,
+            }
+        }
+    }
+
+    let config = Configuration {
+        server: Server::as_server(&server),
+        database: Database::as_database(&database)
+    };
+    Ok(config)
+}
+
+#[derive(Debug)]
+pub struct Configuration {
+    server: Server,
+    database: Database,
+}
+impl Configuration {
+    pub fn new(configuration: Configuration) -> Configuration {
+        Configuration {
+            server: configuration.server,
+            database: configuration.database,
+        }
+    }
+
+}
+pub fn start_application_with_configuration(configuration: Configuration) {
+    println!("{:?}", configuration);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-mod setup; // Import the setup module
-use std::io;
+use cornerstone::{api::start_http_server, run_wizard, setup_check, run_configuration_setup, start_application_with_configuration};
 
-use crate::setup::setup_check;
-use crate::setup::setup_wizard::run_wizard;
 
-fn main() -> io::Result<()> {// Check if the config file exists and is not empty
-    
-    // more implementations for arguments, could even refactor this into it's own enum impl
-
-    if std::env::args().any(|arg| arg == "--setup-wizard") {
-        return run_wizard();
-    } else {
-        return setup_check();
+#[actix_web::main]
+async fn main() {
+    // Create necessary directories
+    if let Err(e) = std::fs::create_dir_all("./tmp") {
+        eprintln!("Failed to create directories: {}", e);
+        return;
     }
-    
+
+    if std::env::args().any(|arg: String| arg == "--setup-wizard") {
+        match run_wizard() {
+            Ok(_) => println!("Setup wizard completed successfully."),
+            Err(e) => eprintln!("Error running setup wizard: {}", e),
+        }
+    } else {
+        match setup_check() {
+            Ok(_) => println!("Setup check completed."),
+            Err(e) => {
+                eprintln!("Error during setup check: {}", e);
+                return;
+            },
+        }
+
+        match run_configuration_setup() {
+            Ok(configuration) => {
+                println!("Starting the application using the specific configuration: ");
+                start_application_with_configuration(configuration);
+            },
+            Err(e) => eprintln!("Error setting up configuration: {}", e),
+        }
+    }
+
+    if let Err(e) = start_http_server().await {
+        eprintln!("Failed to start HTTP server: {}", e);
+    }
 }
